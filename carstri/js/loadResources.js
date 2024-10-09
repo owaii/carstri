19bd806fd7358d93588d60649b839266e81ba078
owaii <118843083+owaii@users.noreply.github.com>
2024-10-09 17:50:06 +0200

    More stuff

--- carstri/js/loadResources.js
+++ carstri/js/loadResources.js
@@ -1,23 +1,22 @@
 function loadScript(src, defer = false) {
     const script = document.createElement('script');
     script.src = src;
-    if (defer) {
-        script.defer = true;
-    }
+    if (defer) script.defer = true;
     document.head.appendChild(script);
 }
 
-function loadStyle(href) {
+function loadStyles(href) {
     const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.href = href;
     document.head.appendChild(link);
 }
 
-loadScript('https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js', true);
-loadScript('https://cdn.tailwindcss.com', true);
-loadScript('./splide/splide.min.js', true);
-loadStyle('./splide/splide.min.css')
-loadStyle('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap')
+/* scripts */
+loadScript('https://cdn.tailwindcss.com');
+loadScript('https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js', true);
+loadScript('./splide/splide.min.js');
 
-
+/* styles */
+loadStyles('./splide/splide.min.css');
+loadStyles('https://fonts.googleapis.com/css2?family=JetBrains+Mono&display=swap');

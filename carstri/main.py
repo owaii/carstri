232743074f886a80fd852611ba02da412c9b0031
ghostper <ghostper964@gmail.com>
2024-10-16 00:08:18 +0200

    new file:   .vs/Carstri-GitHub-Project/FileContentIndex/657037c9-19ae-429b-b861-ee705926b73b.vsidx 	new file:   .vs/Carstri-GitHub-Project/v17/.wsuo     new file:   .vs/Carstri-GitHub-Project/v17/DocumentLayout.json 	new file:   .vs/ProjectSettings.json 	new file:   .vs/VSWorkspaceState.json 	new file:   .vs/slnx.sqlite 	new file:   carstri.db 	deleted:    carstri/includes/__pycache__/iJ.cpython-312.pyc 	deleted:    carstri/includes/__pycache__/sqly.cpython-312.pyc 	deleted:    carstri/includes/__pycache__/std.cpython-312.pyc 	deleted:    carstri/includes/iJ.py 	deleted:    carstri/includes/sqly.py 	deleted:    carstri/includes/std.py 	deleted:    carstri/main.py 	deleted:    carstri/website/__init__.py 	deleted:    carstri/website/__pycache__/__init__.cpython-312.pyc 	deleted:    carstri/website/index.py 	deleted:    carstri/website/webCode/splide/splide.min.css 	deleted:    carstri/website/webCode/splide/splide.min.js 	renamed:    README.md -> docs/README.md 	renamed:    carstri/img/bg/hero_bg.jpg -> img/bg/hero_bg.jpg 	renamed:    carstri/img/bg/hero_bg2.jpg -> img/bg/hero_bg2.jpg 	renamed:    carstri/img/bg/hero_bg3.jpg -> img/bg/hero_bg3.jpg 	renamed:    carstri/img/cars/ae86_trueno.jpg -> img/cars/ae86_trueno.jpg 	renamed:    carstri/img/cars/gtr.jpg -> img/cars/gtr.jpg 	renamed:    carstri/img/cars/silvia_s13.jpg -> img/cars/silvia_s13.jpg 	new file:   main.py 	renamed:    carstri/sql/carstri.sql -> sql/carstri.sql 	renamed:    carstri/website/webCode/assets/css/styles.css -> static/css/styles.css 	renamed:    carstri/website/webCode/assets/js/loadResources.js -> static/js/loadResources.js 	renamed:    carstri/website/webCode/index.html -> templates/index.html 	new file:   templates/inventory.html

--- carstri/main.py
+++
@@ -1,6 +0,0 @@
-from website import * 
-
-app = InitIndex()
-
-if __name__ == '__main__':
-    app.run(debug = True)
--- carstri/website/index.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-index = Blueprint('Carstri', __name__)
-
-@index.root('/')
-def index():
-    return "<h1>Test</h1>"
